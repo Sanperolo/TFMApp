cf9e84fa0b5d50b809127a64b0f1fbb0bf92ea01
Miguel Ángel Herranz Marcos <michaelherranz@gmail.com>
2022-09-27 19:36:04 +0200

    5º commit django Rest Framework installed

---
+++ base/api/views.py
@@ -0,0 +1,27 @@
+from pydoc import render_doc
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from base.models import Room
+from .serializers import RoomSerializers
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes=[
+        'GET /api',
+        'GET /api/rooms',
+        'GET /api/rooms/:id',
+    ]
+    return Response(routes)
+
+@api_view(['GET'])
+def getRooms(request):
+    rooms = Room.objects.all()
+    serializers = RoomSerializers(rooms, many=True)
+    return Response(serializers.data)
+
+
+@api_view(['GET'])
+def getRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    serializers = RoomSerializers(room, many=False)
+    return Response(serializers.data)
